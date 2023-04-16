c029dc84e77a2c19ae7db270c46e139ae6e88269
Andrew Porterfield <ajporterfield@gmail.com>
2023-04-16 15:09:24 -0400

    Adds code comments to action cable monkey patch

--- config/initializers/action_cable.rb
+++ config/initializers/action_cable.rb
@@ -1,23 +1,8 @@
+# Twilio's Media Stream messages do not follow the JSON structure of Action Cable,
+# so we need to transform them in this monkey patch.
+# See https://www.twilio.com/docs/voice/twiml/stream
+
 module ActionCable
-  class TwilioMediaStreamTransformer
-    def transform(data)
-      return unless data["event"].in?(["start", "media"]) && data["streamSid"]
-
-      if data["event"] == "start"
-        {
-          "command" => "subscribe",
-          "identifier" => data.merge("channel" => "MediaStreamsChannel").to_json
-        }
-      else # media
-        {
-          "command" => "message",
-          "identifier" => "{\"channel\": \"MediaStreamsChannel\"}",
-          "data" => data.merge("action" => "media").to_json
-        }
-      end
-    end
-  end
-
   module Connection
     class Subscriptions
       def execute_command(data)
@@ -26,7 +11,7 @@ module ActionCable
         when "unsubscribe" then remove data
         when "message"     then perform_action data
         else
-          handle_unrecognized_command(data)
+          handle_unrecognized_command(data) # this is the only line updated in execute_command
         end
       rescue Exception => e
         @connection.rescue_with_handler(e)
@@ -36,12 +21,33 @@ module ActionCable
       private
 
       def handle_unrecognized_command(data)
-        transformed_data = ActionCable::TwilioMediaStreamTransformer.new.transform(data)
+        transformed_data = transform_data(data)
 
         if transformed_data
           execute_command(transformed_data)
         else
           logger.error "Received unrecognized command in #{data.inspect}"
+        end
+      end
+
+      def transform_data
+        return unless data["event"].in?(["start", "media"]) && data["streamSid"]
+
+        # The start message is transformed into the subscribe command.
+        # See https://www.twilio.com/docs/voice/twiml/stream#message-start
+        if data["event"] == "start"
+          {
+            "command" => "subscribe",
+            "identifier" => data.merge("channel" => "MediaStreamsChannel").to_json
+          }
+        # The media message is routed to the corresponding method in media_streams_channel.rb
+        # See https://www.twilio.com/docs/voice/twiml/stream#message-media
+        else # media
+          {
+            "command" => "message",
+            "identifier" => "{\"channel\": \"MediaStreamsChannel\"}",
+            "data" => data.merge("action" => "media").to_json
+          }
         end
       end
     end
